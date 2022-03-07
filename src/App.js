79506e1996a5ff7c9e88242862378166a22f7be1
unknown <lh77x7@gmail.com>
2022-03-07 19:45:00 +0100

    CoursesList.js in seperate file
    Code refactoring - CoursesList with properties

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import CoursesList from './CoursesList';
 
 const courses = [
   {
@@ -35,21 +36,9 @@ function App() {
       <div>
           <h1>List of Courses</h1>
           <hr />
-          {courses.map(function(course) {
-            return (
-              <div key={course.id}>
-                <span>
-                  <a href={course.url}><h4>{course.title}</h4></a>
-                </span>
-                <span>by <strong>{course.author}</strong></span>
-                <span> | Video Hours: {course.hours_video}</span>
-                <span> | Number of Lectures: {course.number_of_lectures}</span>
-                <span> | Rating: {course.rating}</span>
-                <br/><br/>
-              </div>
-            );
-          })} 
+          <CoursesList courses={courses} />
       </div>
   ); 
 }
+
 export default App;
